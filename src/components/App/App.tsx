6a536abf7be1da67a8b77d47073b7a57b58b25c5
Cameron MacRae <cam.mac.cm@gmail.com>
2020-08-18 10:14:03 -0600

    Add basic imports and methods to all components

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
 import { Route, withRouter } from 'react-router-dom';
-import logo from './logo.svg';
 import './App.scss';
 
-type initialState = {};
+interface initialState {
+  text: string
+};
 
-export class App extends Component<{}, initialState> {
-  state: initialState = {};
+export class App extends Component<initialState> {
+  state: initialState = {
+    text: this.props.text
+  };
 
   componentDidMount = () => {
     document.title = 'MacRae Software';
+    console.log(this.state.text);
   };
 
   render() {
